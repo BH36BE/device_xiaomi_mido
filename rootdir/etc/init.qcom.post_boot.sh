1f51b8fd5a53a10f5b4d57f1f6d6fb720bc819dd
Adam Farden <adam@farden.cz>
2016-09-25 14:38:02 +0200

    mido: rootdir: Configure Zram on all devices, if enabled
    • Don't be afraid! This can give a nice performance boost under heavy load
    • Add zram 1gb
    
    Signed-off-by: Adam Farden <adam@farden.cz>
    Signed-off-by: ItoRenz <ridwan.ri214@gmail.com>
    Change-Id: I8a7a276fcf1c7a84935bca636cf309afd583d2cb

--- rootdir/etc/init.qcom.post_boot.sh
+++ rootdir/etc/init.qcom.post_boot.sh
@@ -341,7 +341,6 @@ function configure_zram_parameters() {
     # For 512MB Go device, size = 384MB, set same for Non-Go.
     # For 1GB Go device, size = 768MB, set same for Non-Go.
     # For >=2GB Non-Go devices, size = 50% of RAM size. Limit the size to 4GB.
-    # And enable lz4 zram compression for Go targets.
 
     RamSizeGB=`echo "($MemTotal / 1048576 ) + 1" | bc`
     zRamSizeBytes=`echo "$RamSizeGB * 1024 * 1024 * 1024 / 2" | bc`
@@ -349,9 +348,10 @@ function configure_zram_parameters() {
         zRamSizeBytes=4294967296
     fi
 
-    if [ "$low_ram" == "true" ]; then
-        echo lz4 > /sys/block/zram0/comp_algorithm
-    fi
+    # Setup zram options
+    echo lz4 > /sys/block/zram0/comp_algorithm
+    echo 8   > /sys/block/zram0/max_comp_streams
+    echo 0   > /proc/sys/vm/page-cluster
 
     if [ -f /sys/block/zram0/disksize ]; then
         if [ -f /sys/block/zram0/use_dedup ]; then
@@ -362,7 +362,7 @@ function configure_zram_parameters() {
         elif [ $MemTotal -le 1048576 ]; then
             echo 805306368 > /sys/block/zram0/disksize
         else
-            echo $zRamSizeBytes > /sys/block/zram0/disksize
+            echo 1073741824 > /sys/block/zram0/disksize
         fi
 
         # ZRAM may use more memory than it saves if SLAB_STORE_USER
