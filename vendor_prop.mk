6e44e560cc1e145df9f73dc53b8652968c157859
Nich <nctrenco@gmail.com>
2019-07-10 02:53:03 +0200

    mido: Set virtual displays to 0
    We don't have any virtual displays, however HWC assumes 1 virtual
    display by default. This was causing laggy display when allocating
    virtual displays, as done by surfaceflinger during screen recording
    for example.
    
    Change-Id: Iab7ddb4182ee1d952f3083942eece033f651af98
    Signed-off-by: David Trpchevski <trpcevski.david@gmail.com>

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -68,6 +68,7 @@ debug.cpurend.vsync=false \
 debug.egl.hw=0 \
 debug.enable.sglscale=1 \
 debug.mdpcomp.logs=0 \
+debug.sdm.support_writeback=0 \
 debug.sf.enable_hwc_vds=1 \
 debug.sf.hw=0 \
 debug.sf.latch_unsignaled=1 \
