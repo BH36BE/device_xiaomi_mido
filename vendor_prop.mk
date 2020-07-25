92051a8f0d732ace9917aa8604271ed9871b2750
baalajimaestro <me@baalajimaestro.me>
2020-07-25 19:03:29 +0530

    mido: Kang dexpreopt bits from OnePlus 7 tree
    Speeds up first boot insanely
    
    Signed-off-by: baalajimaestro <me@baalajimaestro.me>
    Signed-off-by: CryllicBuster273 <cryllicbuster273@pixelexperience.org>

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -1,6 +1,16 @@
 #
 # system.prop for mido
 #
+# ART
+pm.dexopt.ab-ota=speed-profile \
+dalvik.vm.dex2oat-filter=speed \
+dalvik.vm.image-dex2oat-filter=speed \
+ro.vendor.qti.am.reschedule_service=true \
+ro.sys.fw.dex2oat_thread_count=8 \
+dalvik.vm.boot-dex2oat-threads=8 \
+dalvik.vm.dex2oat-threads=4 \
+dalvik.vm.image-dex2oat-threads=4
+
 # Audio
 PRODUCT_PROPERTY_OVERRIDES += \
 af.fast_track_multiplier=2 \
