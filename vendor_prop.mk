ae91f509463e1f0cf627df2c2dd65c687aef6820
Randall Huang <huangrandall@google.com>
2021-06-21 20:44:01 +0200

    mido: overlay: Enable zram-writeback job
    This patch enables zram-writeback job. It will run idle marking
    at 60m since the device is boot. After that, it will do first
    writeback after 3hour to sweep initiallized cold pages out.
    After that, it periodically write out every 24hour.
    
    Bug: 117682284
    Bug: 127185055
    Test: confirm with manual testing
    Change-Id: I6857c79ca3ce6a3619f4b7e200d7f17a8decd659
    Signed-off-by: Randall Huang <huangrandall@google.com>

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -225,3 +225,9 @@ persist.dirac.acs.storeSettings=1 \
 persist.dirac.acs.ignore_error=1 \
 ro.audio.soundfx.dirac=true \
 persist.audio.dirac.speaker=true
+
+# ZRAM
+PRODUCT_PROPERTY_OVERRIDES += \
+ro.zram.mark_idle_delay_mins=60 \
+ro.zram.first_wb_delay_mins=180 \
+ro.zram.periodic_wb_delay_hours=24
