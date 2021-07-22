2473baeacc349d0577b5a251637095660f3984d9
ugly-kid-af <dwarmachine24@gmail.com>
2021-07-22 11:56:46 +0530

    mido: Remove packages: Spare Some apps
    Signed-off-by: ugly-kid-af <dwarmachine24@gmail.com>
    Signed-off-by: smokey18 <kmboplays@gmail.com>

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -4,11 +4,11 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePkgs
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := AmbientSensePrebuilt AndroidAutoStubPrebuilt AppDirectedSMSService Camera2 ConnMO DCMO
-LOCAL_OVERRIDES_PACKAGES += DMService GCS GoogleCamera MaestroPrebuilt Maps MicropaperPrebuilt MyVerizonServices
-LOCAL_OVERRIDES_PACKAGES += OBDM_Permissions Ornament SafetyHubPrebuilt SCONE ScribePrebuilt Showcase Snap
-LOCAL_OVERRIDES_PACKAGES += SoundAmplifierPrebuilt SprintDM SprintHM TurboPrebuilt Tycho USCCDM VZWAPNLib
-LOCAL_OVERRIDES_PACKAGES += VzwOmaTrigger WallpapersBReel2020 arcore obdm_stub DevicePolicyPrebuilt RecorderPrebuilt
+LOCAL_OVERRIDES_PACKAGES := AmbientSensePrebuilt AppDirectedSMSService Camera2 CarrierSetup ConnMO DCMO
+LOCAL_OVERRIDES_PACKAGES += DMService Drive GCS GoogleCamera MaestroPrebuilt Maps MicropaperPrebuilt MyVerizonServices
+LOCAL_OVERRIDES_PACKAGES += OBDM_Permissions Ornament Photos PixelLiveWallpaperPrebuilt PrebuiltGmail SafetyHubPrebuilt SCONE ScribePrebuilt Showcase Snap
+LOCAL_OVERRIDES_PACKAGES += SoundAmplifierPrebuilt SprintDM SprintHM Tycho USCCDM VZWAPNLib Videos
+LOCAL_OVERRIDES_PACKAGES += VzwOmaTrigger WallpapersBReel2020 YouTube YouTubeMusicPrebuilt arcore obdm_stub DevicePolicyPrebuilt RecorderPrebuilt
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := platform
 LOCAL_SRC_FILES := /dev/null
