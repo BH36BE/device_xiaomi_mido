3a996671ab72e9ea24be198575d5d7a971adcdca
Nauval Rizky <enuma.alrizky@gmail.com>
2021-10-14 20:07:35 +0000

    mido: rootdir: Add script for exTHmUI's GameMode performance control
    This is based on samples at exTHmUI-supported device (link down below),
    but without turing-complete language involvements since we're only
    setting up sysfs nodes.
    
    [fryevia: correct wrong thermal value and add preset for inactive mode]
    
    Link: https://github.com/exthmui-devices/android_device_xiaomi_vince/commit/dc18f8e95054e172e26efce984b15896f716daf8
    Signed-off-by: Nauval Rizky <enuma.alrizky@gmail.com>
    Signed-off-by: David Setiawan <valkry24@gmail.com>
    Signed-off-by: alanndz <alanndz@qmail.id>

--- device.mk
+++ device.mk
@@ -340,6 +340,7 @@ PRODUCT_PACKAGES += \
     init.msm.usb.configfs.rc \
     init.qcom.early_boot.sh \
     init.qcom.post_boot.sh \
+    init.performance_level.rc \
     init.qcom.rc \
     init.qcom.sh \
     init.qcom.usb.rc \
