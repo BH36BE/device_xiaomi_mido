9d4e1cde34186caefe5069156123463cd745f1f6
ikeh045 <njkampret@gmail.com>
2021-11-09 00:47:13 +0000

    Nusantara ify * drop lineage stuff

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_mido.mk
+    $(LOCAL_DIR)/nad_mido.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_mido-user \
-    lineage_mido-userdebug \
-    lineage_mido-eng
+    nad_mido-user \
+    nad_mido-userdebug \
+    nad_mido-eng
